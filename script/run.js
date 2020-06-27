c70af1a5c917ae04ff6e1040cd73183620fa202a
Kk Shinkai <kkshinkai@outlook.com>
2020-06-27 20:23:14 +0800

    ..

--- script/run.js
+++ script/run.js
@@ -21,8 +21,8 @@ let template = ({title, date, author, body}) => `<!DOCTYPE html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-    <title>$title$ &middot; Kk Shinkai</title>
-    <link rel="stylesheet" href="../static/kkshinkai-v1.css">
+    <title>${title} &middot; Kk Shinkai</title>
+    <link rel="stylesheet" href="../static/kkshinkai-v2.css">
 </head>
 <body>
 <h1>${title}</h1>
